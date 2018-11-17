65b2baaafcebaa55e517b74abefa2a3818f96763
SirWadefield <sirwadefield@gmail.com>
2018-11-17 01:31:07 -0600

    floor visual assets added

---
+++ WPFGame/World/Stage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFGame.World
+{
+    class Stage
+    {
+        //BitmapImage imga = new BitmapImage(new Uri("../../VisualAssets/Tiles/floor_light.png", UriKind.Relative));
+        //WriteableBitmap bma = new WriteableBitmap(imga);
+        //surface.Blit(new Point(0, 472), bma, new Rect(new Size(800, 100)), Colors.White, WriteableBitmapExtensions.BlendMode.Alpha);
+    }
+}
