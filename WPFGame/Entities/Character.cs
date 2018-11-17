1661d98a92dbb798eadb9c61ede6799fc37c72e3
Trever Hall <treahall@ut.utm.edu>
2018-11-17 00:09:39 -0600

    Added jumping

--- WPFGame/Entities/Character.cs
+++ WPFGame/Entities/Character.cs
@@ -8,11 +8,15 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using WPFGame.GameEntities;
+using WPFGame;
 
 namespace WPFGame.Entities
 {
     public class Character : GameEntity
     {
+        
+        int health;
+        int maxspeed = 240;
         public override void Draw(WriteableBitmap surface)
         {
             BitmapImage img = new BitmapImage(new Uri("C:\\Users\\Trever\\source\\repos\\WPFGame\\WPFGame\\Entities\\34.png", UriKind.Relative));
@@ -22,18 +26,51 @@ namespace WPFGame.Entities
             base.Draw(surface);
         }
 
+        bool jumping = false;
+        int gforce = -120;
+        int force;
         public override void move()
         {
+
             if(Keyboard.IsKeyDown(Key.Left))
             {
-                Velocity = new System.Numerics.Vector2(-120, 0);
+                if(Position.X >= 0) { Velocity = new System.Numerics.Vector2(-120, Velocity.Y); }
+                
             }
             else if (Keyboard.IsKeyDown(Key.Right))
             {
-                Velocity = new System.Numerics.Vector2(120, 0);
+                if(Position.X <= 400) { Velocity = new System.Numerics.Vector2(maxspeed, Velocity.Y); }
+                
             }
             else
-                Velocity = new System.Numerics.Vector2(0, 0);
+                Velocity = new System.Numerics.Vector2(0, Velocity.Y);
+
+            //sperate for independent jumping action
+            if (Keyboard.IsKeyDown(Key.Space))
+            {
+                if (jumping == false)
+                {
+                    jumping = true;
+                    force = gforce;
+                }
+
+            }
+            //allows character to jump with acceleration and deceleration
+            if (jumping == true)
+            {
+                Velocity += new System.Numerics.Vector2(0, force);
+                if(force < 120)
+                    force += 12;
+            }
+
+            //Stop falling at the bottom
+            if(Position.Y > 250 && jumping == true)
+            {
+                Velocity = new System.Numerics.Vector2(Velocity.X, 0); // sets vertical velocity to zero
+                Position = new System.Numerics.Vector2(Position.X, 250); // resets the base vertical position
+                jumping = false; //stops jumping 
+                force = gforce;
+            }
         }
 
         public override void GameTick(float millisecondsPassed)
